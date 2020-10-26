bc9bda675f959b446e0ad80083a9f8d9db8ecabe
s1x-0f-cr0ws <73398232+s1x-0f-cr0ws@users.noreply.github.com>
2020-10-26 13:14:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,7 @@
 // btw i have no idea who those two people are maybe K and V...or K and C...or C and V *shrugs* - sophia
 function signUp1() {
     var uname = document.getElementsByName("username")[0].value;
+    console.log(document.getElementsByName("username")[0].value)
     var pword = document.getElementsByName("psw")[0].value;
     document.cookie = "username="+encodeURIComponent(hash(uname))+"; path=/; max-age=31536000";
     document.cookie = "password="+encodeURIComponent(hash(uname))+"; path=/; max-age=31536000";
