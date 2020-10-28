2afdf1ccf711bcb7949105f591407fc806899adb
s1x-0f-cr0ws <73398232+s1x-0f-cr0ws@users.noreply.github.com>
2020-10-28 13:20:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -28,70 +28,56 @@ function signUp1() {
     var pword = document.getElementsByName("psw")[0].value;
     document.cookie = "username="+encodeURIComponent(hash(uname))+"; path=/; max-age=31536000";
     document.cookie = "password="+encodeURIComponent(hash(uname))+"; path=/; max-age=31536000";
-    var signalert = "You've signed up! Snakespeare is happy ðŸ¥°";
+    var signalert = "You've signed up! Snakespeare is happy Ã°Å¸Â¥Â°";
     alert(signalert);
+    document.cookie = "login=; path=/; expires=Thu 01 Jan 1970 00:00:00 GMT";
+    if (document.getElementsByName("remember")[0].checked) {
+	document.cookie = "login="+encodeURIComponent(uname)+"; path=/; max-age=2419200";
+    }
     window.location.replace = "https://en.wikipedia.org/wiki/HTTP_404";
     return uname;
     return pword;
 }
 
-function signUp2() {
-    var uname = document.getElementsByName("username")[0].value;
-    var pword = document.getElementsByName("psw")[0].value;     
-    localStorage.setItem("username", encodeURIComponent(hash(uname)));
-    localStorage.setItem("password", encodeURIComponent(hash(uname)));
-}
-
-
-			function login1() {
-        var uname = document.getElementsByName("username")[0].value;
-    var pword = document.getElementsByName("psw")[0].value;
-
-				var cooks = document.cookie;
+function login1() {
+    var cooks = document.cookie;
     var c = [cooks.indexOf("username="), cooks.indexOf("password=")];
     c[0] = cooks.substring((c[0]+9), cooks.length);
     c[1] = cooks.substring((c[1]+9), cooks.length);
     c[0] = c[0].substring(0, c[0].indexOf(";"));
     c[1] = c[1].substring(0, c[1].indexOf(";"));
 
-    var myBool = (hash(uname) == c[0]);
-    if ((hash(pword) == c[1]) && myBool) {
+    var myBool = (hash(uname) == decodeURIComponent(c[0]));
+    if ((hash(pword) == decodeURIComponent(c[1])) && myBool) {
 	myBool = true;
     } else {
 	myBool = false;
     }
 
-    if (myBool = true) {
-	alert("If you're redirected to your profile after you close this alert, then congrats, and welcome back, " + uname + "! Go do some reading! 📚 \n\nIf you aren't, your username and/or password is incorrect...maybe you put a blank space in any of the fields...Snakespeare is sad 😭");
+    if (myBool) {
+	alert("Successfully logged in! Welcome back, " + uname + "! If you want to access your profile, go to https://sssread.netlify.app/profile.html. Now, go do some reading! ðŸ“š");
+	document.cookie = "login=; path=/; expires=Thu 01 Jan 1970 00:00:00 GMT";
+	document.cookie = "login="+encodeURIComponent(uname)+"; path=/; max-age=2419200";
     } else if (myBool = false) {
-	alert("If you're redirected to your profile after you close this alert, then congrats, and welcome back, " + uname + "! Go do some reading! 📚 \n\nIf you aren't, your username and/or password is incorrect...maybe you put a blank space in any of the fields...Snakespeare is sad 😭");
+	alert("Your username and/or password is incorrect...maybe you put a blank space in one of the fields...Snakespeare is sad ðŸ˜­");
     } else {
-	 alert("You shouldn't be getting this alert....uh oh 😖");   
+	 alert("You shouldn't be getting this alert....uh oh ðŸ˜–");   
     }
 	return uname;
 	return pword;
 }
 
-function login2(form) {
-    form.action = "profile.html";
-	var uname = document.getElementsByName("username")[0].value;
-    var pword = document.getElementsByName("psw")[0].value;
+function profile() {
+	var username = document.cookie;
+	username = username.substring((username.indexOf("login=")+6), username.length);
+	username = username.substring(0, username.indexOf(";"));
+	username = decodeURIComponent(username);
 
-    var myBool = (hash(uname).toString() == (localStorage.getItem("username")));
-
-    if ((hash(pword).toString() == (localStorage.getItem("password"))) && myBool) {
-	myBool = true;
-    } else {
-	myBool = false;
-    }
-	if(myBool = true) {
-		alert("Successfully logged in! Welcome back, " + uname + "! If you want to access your profile, go to https://sssread.netlify.app/profile.html. Now, go do some reading! 📚");
-	window.location.href = "https://sssread.netlify.app/profile.html";
+	if (username == "") {
+		window.location.href = "login.html";
+	} else {
+		document.querySelectorAll('p')[6].innerHTML = "<strong>Username:</strong> "+username;
 	}
-	else {
-		alert("If you're redirected to your profile after you close this alert, then congrats! Go do some reading! 📚 /n If you aren't, your username and/or password is incorrect...Snakespeare is sad 😭"); 	
-	}
-	return uname;
-	return pword;
 }
+
 // THANK YOU SO MUCH NATHAN!!! :)))))
