a937ea47a002a21c147e0a7ae504b346e7684a8f
s1x-0f-cr0ws <73398232+s1x-0f-cr0ws@users.noreply.github.com>
2022-08-30 12:01:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,11 +18,7 @@
 
 // btw i have no idea who those two people are maybe K and V...or K and C...or C and V *shrugs* - sophia
 
-var editorExtensionId = "oendekmpdaaepnnonbppbpcmhjjdnign";
 
-chrome.runtime.sendMessage(editorExtensionId, {message: "hi"}, (response) => {
-  console.log(response.message);
-}); 
 
 /*chrome.runtime.sendMessage(editorExtensionId, {openUrlInEditor: url},
   function(response) {
